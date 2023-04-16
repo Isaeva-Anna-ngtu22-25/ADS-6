1c70d66e4c547cd1051c68c98dd6e7f0b7851318
Isaeva Anna <116427105+Isaeva-Anna-ngtu22-25@users.noreply.github.com>
2023-04-16 23:21:24 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,22 +1,27 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
     T *arr;
     int head, tail, count;
+    
  public:
     TPQueue():head(0), tail(0), count(0) {
         arr = new T[size];
     }
+    
     ~TPQueue() {
         delete[] arr;
     }
+    
     bool isEmpty() const {
         return count == 0;
     }
+    
     void push(const T& value) {
         if (size != count) {
             int per = tail;
@@ -34,6 +39,7 @@ class TPQueue {
             count++;
         }
     }
+    
     T pop() {
         if (isEmpty()) {
             throw std::string("Empty!");
